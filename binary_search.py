30554f8090bb540094d7ca2c3d76965126b1a83b
gl1n0m3c <94142724+gl1n0m3c@users.noreply.github.com>
2023-07-31 20:28:58 +0300

    Update binary_search.py
    leetcode_1

--- binary_search.py
+++ binary_search.py
@@ -16,3 +16,29 @@ def binary_search(list, item):
 my_list = [1, 3, 5, 7, 9]
 print(binary_search(my_list, 3))
 print(binary_search(my_list, -1))
+
+
+
+# Задачка с leetcode на бинарный поиск https://leetcode.com/problems/search-in-rotated-sorted-array/description/ (medium)
+def BinSearch(nums, target):
+    left = 0
+    right = len(nums) - 1
+    print(left, right)
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        else:
+            if nums[mid] >= nums[left]:  # перехода не было
+                if nums[left] <= target < nums[mid] :
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:                        # переход был
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        print(left, right)
+        print()
+    return -1  # число не найдено
